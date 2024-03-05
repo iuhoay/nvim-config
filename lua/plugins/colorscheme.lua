c5b4b5ab7779c9cfad88cfe212c7efb5886ad062
iuhoay <iuhoay@gmail.com>
2024-03-05 13:42:59 +0800

    Colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
+}
